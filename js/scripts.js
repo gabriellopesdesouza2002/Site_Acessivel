a3bdca117c3cab93a55d029cf56b420f7ca05f99
Gabriel Lopes <gabriellopesdesouza2002@gmail.com>
2021-10-23 21:09:52 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,185 @@
+//Letra A
+let la = new Audio();
+la.src = 'songs/alfabeto-pt-br/a.mp3';
+
+//Letra B
+let lb = new Audio();
+lb.src = 'songs/alfabeto-pt-br/b.mp3';
+
+//Letra C
+let lc = new Audio();
+lc.src = 'songs/alfabeto-pt-br/c.mp3';
+
+//Letra D
+let ld = new Audio();
+ld.src = 'songs/alfabeto-pt-br/d.mp3';
+
+//Letra E
+let le = new Audio();
+le.src = 'songs/alfabeto-pt-br/e.mp3';
+
+//Letra F
+let lf = new Audio();
+lf.src = 'songs/alfabeto-pt-br/f.mp3';
+
+//Letra G
+let lg = new Audio();
+lg.src = 'songs/alfabeto-pt-br/g.mp3';
+
+//Letra H
+let lh = new Audio();
+lh.src = 'songs/alfabeto-pt-br/h.mp3';
+
+//Letra I
+let li = new Audio();
+li.src = 'songs/alfabeto-pt-br/i.mp3';
+
+//Letra J
+let lj = new Audio();
+lj.src = 'songs/alfabeto-pt-br/j.mp3';
+
+//Letra K
+let lk = new Audio();
+lk.src = 'songs/alfabeto-pt-br/k.mp3';
+
+//Letra L
+let ll = new Audio();
+ll.src = 'songs/alfabeto-pt-br/l.mp3';
+
+//Letra M
+let lm = new Audio();
+lm.src = 'songs/alfabeto-pt-br/m.mp3';
+
+//letra N
+let ln = new Audio();
+ln.src = 'songs/alfabeto-pt-br/n.mp3';
+
+//letra O
+let lo = new Audio();
+lo.src = 'songs/alfabeto-pt-br/o.mp3';
+
+//letra P
+let lp = new Audio();
+lp.src = 'songs/alfabeto-pt-br/p.mp3';
+
+//letra Q
+let lq = new Audio();
+lq.src = 'songs/alfabeto-pt-br/q.mp3';
+
+//letra R
+let lr = new Audio();
+lr.src = 'songs/alfabeto-pt-br/r.mp3';
+
+//letra S
+let ls = new Audio();
+ls.src = 'songs/alfabeto-pt-br/s.mp3';
+
+//letra T
+let lt = new Audio();
+lt.src = 'songs/alfabeto-pt-br/t.mp3';
+
+//letra U
+let lu = new Audio();
+lu.src = 'songs/alfabeto-pt-br/u.mp3';
+
+//letra V
+let lv = new Audio();
+lv.src = 'songs/alfabeto-pt-br/v.mp3';
+
+//letra W
+let lw = new Audio();
+lw.src = 'songs/alfabeto-pt-br/w.mp3';
+
+//letra X
+let lx = new Audio();
+lx.src = 'songs/alfabeto-pt-br/x.mp3';
+
+//letra Y
+let ly = new Audio();
+ly.src = 'songs/alfabeto-pt-br/y.mp3';
+
+//letra Z
+let lz = new Audio();
+lz.src = 'songs/alfabeto-pt-br/z.mp3';
+
+//Inglês Dia-a-Dia
+//Bike
+let lbike = new Audio();
+lbike.src = '../songs/dia-a-dia-en/bike.mp3'
+
+//Book
+let lbook = new Audio();
+lbook.src = '../songs/dia-a-dia-en/book.mp3'
+
+//Camera
+let lcamera = new Audio();
+lcamera.src = '../songs/dia-a-dia-en/camera.mp3'
+
+//car
+let lcar = new Audio();
+lcar.src = '../songs/dia-a-dia-en/car.mp3'
+
+//cat
+let lcat = new Audio();
+lcat.src = '../songs/dia-a-dia-en/cat.mp3'
+
+//cell
+let lcell = new Audio();
+lcell.src = '../songs/dia-a-dia-en/cell.mp3'
+
+//chair
+let lchair = new Audio();
+lchair.src = '../songs/dia-a-dia-en/chair.mp3'
+
+//clock
+let lclock = new Audio();
+lclock.src = '../songs/dia-a-dia-en/clock.mp3'
+
+//dog
+let ldog = new Audio();
+ldog.src = '../songs/dia-a-dia-en/dog.mp3'
+
+//guitar
+let lguitar = new Audio();
+lguitar.src = '../songs/dia-a-dia-en/guitar.mp3'
+
+//bible
+let lbible = new Audio();
+lbible.src = '../songs/dia-a-dia-en/bible.mp3'
+
+//hospital
+let lhospital = new Audio();
+lhospital.src = '../songs/dia-a-dia-en/hospital.mp3'
+
+//iron
+let liron = new Audio();
+liron.src = '../songs/dia-a-dia-en/iron.mp3'
+
+//kettle
+let lkettle = new Audio();
+lkettle.src = '../songs/dia-a-dia-en/kettle.mp3'
+
+//mask
+let lmask = new Audio();
+lmask.src = '../songs/dia-a-dia-en/mask.mp3'
+
+//notebook
+let lnotebook = new Audio();
+lnotebook.src = '../songs/dia-a-dia-en/notebook.mp3'
+
+//shower
+let lshower = new Audio();
+lshower.src = '../songs/dia-a-dia-en/shower.mp3'
+
+//television
+let ltelevision = new Audio();
+ltelevision.src = '../songs/dia-a-dia-en/television.mp3'
+
+//toilet
+let ltoilet = new Audio();
+ltoilet.src = '../songs/dia-a-dia-en/toilet.mp3'
+
+//tree
+let ltree = new Audio();
+ltree.src = '../songs/dia-a-dia-en/tree.mp3'
+
